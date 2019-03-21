fd0e0291543655b6b4657e86c4257fed9787ec53
saiks24 <saiks1337@yandex.ru>
2019-03-21 12:49:53 +0300

    Add methods to RpcClientInterface

--- src/Client/RpcClientInterface.php
+++ src/Client/RpcClientInterface.php
@@ -3,7 +3,33 @@
 namespace Saiks24\Rpc\Client;
 
 
+use Saiks24\Rpc\Request\Request;
+use Saiks24\Rpc\Response\RpcResponse;
+
+/** Base JSON RPC Client Interface
+ * Interface RpcClientInterface
+ *
+ * @package Saiks24\Rpc\Client
+ */
 interface RpcClientInterface
 {
 
+    /** Set target to request
+     * @param String $address (Example: 0.0.0.0 http://sitename.com )
+     *
+     * @return mixed
+     */
+    public function setRequestAddress(String $address);
+
+    /** Set Body for request
+     * @param \Saiks24\Rpc\Request\Request $request
+     *
+     * @return mixed
+     */
+    public function setRequestBody(Request $request);
+
+    /** Send request and get RpcResponse
+     * @return \Saiks24\Rpc\Response\RpcResponse
+     */
+    public function sendRequest() : RpcResponse;
 }
