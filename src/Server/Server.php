4cd84dbbdf2f60a4320fd7c9848ccfe41ed21517
saiks24 <saiks1337@yandex.ru>
2019-03-22 14:16:36 +0300

    Add documentation to Server

--- src/Server/Server.php
+++ src/Server/Server.php
@@ -15,11 +15,21 @@ class Server implements RpcServerInterface
     /** @var \Saiks24\Rpc\Response\RpcResponse */
     private $response;
 
+    /** Set response body
+     * @param \Saiks24\Rpc\Response\RpcResponseInterface $response
+     *
+     * @return mixed|void
+     */
     public function setResponse(RpcResponseInterface $response)
     {
         $this->response = $response;
     }
 
+    /** Write Response in stream
+     * @param \Psr\Http\Message\StreamInterface $stream
+     *
+     * @return bool
+     */
     public function sendResponse(StreamInterface $stream): bool
     {
         $stream->write($this->response->serialize());
