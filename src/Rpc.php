eb0590a9e8119c65386faa39fa2d5e61d5a50c9c
saiks24 <saiks1337@yandex.ru>
2019-03-25 11:43:42 +0300

    Add Rpc::validateRequest and Rpc::validateResponse methods

--- src/Rpc.php
+++ src/Rpc.php
@@ -7,4 +7,50 @@ class Rpc
     const RPC_PROTOCOL_VERSION_2_0 = '2.0';
     const RPC_RESPONSE_STATUS_SUCCESS = 'success';
     const RPC_RESPONSE_STATUS_ERROR = 'error';
+
+    /** Validate that input string - is correct JSON-RPC Request
+     * @param string $request
+     *
+     * @return bool
+     */
+    public static function validateRequest(string $request) : bool
+    {
+        $request = json_decode($request,true);
+        if(empty($request)) {
+            return false;
+        }
+        if(!isset($request['jsonrpc'])) {
+            return false;
+        }
+        if(!isset($request['method'])) {
+            return false;
+        }
+        if(!isset($request['params'])) {
+            return false;
+        }
+        return true;
+    }
+
+    /** Validate that input string is - correct JSON-RPC response
+     * @param string $response
+     *
+     * @return bool
+     */
+    public static function validateResponse(string $response) : bool
+    {
+        $response = json_decode($response,true);
+        if(empty($response)) {
+            return false;
+        }
+        if(!isset($response['jsonrpc'])) {
+            return false;
+        }
+        if(!isset($response['error']) && !isset($response['result'])) {
+            return false;
+        }
+        if(isset($response['error']) && gettype($response['error']) !== 'array') {
+            return false;
+        }
+        return true;
+    }
 }
